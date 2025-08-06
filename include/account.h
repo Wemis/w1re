5469f3d9393ce6da128c11848c7fc101a14c48db
EndeavourOS <denaderkac@gmail.com>
2025-08-06 17:32:22 +0300

    account verification

--- include/account.h
+++ include/account.h
@@ -5,5 +5,6 @@
 #include <stdint.h>
 
 User get_account(uint8_t privkey[32], char username[17], char name[32]);
+int verify_account(User user);
 
 #endif
--- src/client/main.c
+++ src/client/main.c
@@ -1,47 +1,9 @@
 #include "../../include/account.h"
 #include <arpa/inet.h>
 #include <stdio.h>
-#include <string.h>
-
-uint8_t hexCharToValue(char c) {
-    if ('0' <= c && c <= '9') return c - '0' ;
-    if ('a' <= c && c <= 'f') return c - 'a' + 10;
-    if ('A' <= c && c <= 'F') return c - 'A' + 10;
-    return 0xFF;
-}
-
-size_t hex_to_bytes(const char* hexStr, uint8_t* outBytes, size_t maxLen) {
-    size_t len = strlen(hexStr);
-    if (len % 2 != 0) {
-        return -1;
-    }
-
-    size_t byteCount = len / 2;
-    if (byteCount > maxLen) {
-        return -1;
-    }
-
-    for (size_t i = 0; i < byteCount; ++i) {
-        uint8_t high = hexCharToValue(hexStr[2 * i]);
-        uint8_t low  = hexCharToValue(hexStr[2 * i + 1]);
-
-        if (high == 0xFF || low == 0xFF) {
-            return -1;
-        }
-
-        outBytes[i] = (high << 4) | low;
-    }
-
-    return byteCount;
-}
+#include "../utils/hex.h"
 
 
-void print_hex(uint8_t* hex, int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%02x", hex[i]);
-    }
-    printf("\n");
-}
 
 int main(void) {
     char* usrname = "danylo";
@@ -67,4 +29,7 @@ int main(void) {
 
     printf("\nSignature: ");
     print_hex(usr.signature, 64);
+
+    int r = verify_account(usr);
+    printf("\nVerification result: %d\n", r);
 }
